843b7e4f173cbbeb6229a46f595fe60d4f1c7bbd
Shah Newaj Rabbi Shishir <shahshishir.09@gmail.com>
2018-04-04 20:37:10 +0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,53 @@
+/***
+
+            Bismillahir Rahmanir Rahim
+            Read the name of Allah, who created you!!!
+            Author : Shah Newaj Rabbi Shishir
+            Department of CSE, City University, Bangladesh.
+
+***/
+
+import java.util.Scanner;
+import java.math.BigInteger;
+
+public class Main
+{
+	public static BigInteger GCD (BigInteger a, BigInteger b)
+	{
+		if (b.equals(BigInteger.valueOf(0)))
+			return a;
+		else
+			return GCD (b,a.mod(b));
+	}
+
+	public static void main (String[] args)
+	{
+		Scanner in = new Scanner (System.in);
+		int t,pos,n,i;
+		BigInteger gcd,lcm;
+
+		t = in.nextInt();
+
+		for (pos=1; pos<=t; pos++)
+		{
+			n = in.nextInt();
+
+			int []arr = new int [n];
+
+			for (i=0; i<n; i++)
+				arr[i] = in.nextInt();
+
+			lcm = BigInteger.valueOf(arr[0]);
+
+			for (i=1; i<n; i++)
+			{
+				gcd = GCD (lcm,BigInteger.valueOf(arr[i]));
+				lcm = (lcm.divide(gcd)).multiply(BigInteger.valueOf(arr[i]));
+			}
+
+			System.out.println ("Case " +pos+ ": " +lcm);
+
+			System.gc ();
+		}
+	}
+}
